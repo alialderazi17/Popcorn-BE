30bf3d02794853ccbb7d6ac44555cc7be3773b26
abeer-rozba <abeerrozba3@gmail.com>
2026-04-19 14:29:10 +0300

    create authRouter and authController + adjusted passwordDigest in User model

---
+++ controllers/authController.js
@@ -0,0 +1,104 @@
+const { User } = require('../models/User')
+const middleware = require('../middleware')
+
+const register = async (req, res) => {
+  try {
+    const { username, email, password } = req.body
+    let passwordDigest = await middleware.hashPassword(password)
+    let existingUser = await User.exists({ email })
+
+    if (existingUser) {
+      return res.status(400).send('A user with this email already exists.')
+    } else {
+      const user = await User.create({ username, email, passwordDigest })
+      res.send(user)
+    }
+  } catch (error) {
+    return res.status(401).send({
+      status: 'Error',
+      msg: 'An error has occurred while registering this user!'
+    })
+  }
+}
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body
+    const user = await User.findOne({ email })
+    let matched = await middleware.comparePassword(
+      password,
+      user.passwordDigest
+    )
+    if (matched) {
+      let payload = {
+        id: user._id,
+        email: user.email,
+        username: user.username
+      }
+      let token = middleware.createToken(payload)
+      let userData = {
+        id: user._id,
+        email: user.email,
+        username: user.username
+      }
+      return res.send({ user: userData, token })
+    } else {
+      return res.status(401).send({
+        status: 'Error',
+        msg: 'Incorrect password!'
+      })
+    }
+  } catch (error) {
+    return res.status(401).send({
+      status: 'Error',
+      msg: 'An error has occurred while logging in!'
+    })
+  }
+}
+
+const updatePassword = async (req, res) => {
+  try {
+    const { oldPassword, newPassword } = req.body
+    let user = await User.findById(req.params.id)
+    let matched = await middleware.comparePassword(
+      oldPassword,
+      user.passwordDigest
+    )
+    if (matched) {
+      let passwordDigest = await middleware.hashPassword(newPassword)
+      user = await User.findByIdAndUpdate(req.params.id, {
+        passwordDigest
+      })
+      let payload = {
+        id: user._id,
+        email: user.email,
+        username: user.username
+      }
+      return res
+        .status(200)
+        .send({ status: 'Password updated!', user: payload })
+    } else {
+      return res.status(401).send({
+        status: 'Error',
+        msg: 'Incorrect password!'
+      })
+    }
+  } catch (error) {
+    return res.status(401).send({
+      status: 'Error',
+      msg: 'An error has occurred while updating the password!'
+    })
+  }
+}
+
+const checkSession = async (req, res) => {
+  const { payload } = res.locals
+  res.status(200).send(payload)
+}
+
+module.exports = {
+  register,
+  login,
+  updatePassword,
+  checkSession
+}
--- models/User.js
+++ models/User.js
@@ -1,25 +1,24 @@
-const mongoose = require("mongoose")
+const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema(
   {
     username: {
       type: String,
-      required: true,
+      required: true
     },
     email: {
       type: String,
-      required: true,
+      required: true
     },
-    password: {
+    passwordDigest: {
       type: String,
-      required: true,
+      required: true
     },
     profilePic: {
-      type: String,
-      required: true,
-    },
+      type: String
+    }
   },
   { timestamps: true }
 )
 
-module.exports = mongoose.model("User", userSchema)
+module.exports = mongoose.model('User', userSchema)
