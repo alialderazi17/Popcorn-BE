916f5c6661cbc51f5ddb0b9c7b71b12d1867cc7f
abeer-rozba <abeerrozba3@gmail.com>
2026-04-19 11:49:35 +0300

    create Media and MediaList models

---
+++ models/Media.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+const mediaSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    releaseDate: {
+      type: Date,
+      required: true
+    },
+    mediaType: {
+      type: String,
+      required: true
+    },
+    rating: {
+      type: Number,
+      required: true
+    },
+    image: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    timestamps: true
+  }
+)
+
+module.exports = mongoose.model('Media', mediaSchema)
---
+++ models/MediaList.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const mediaListSchema = new mongoose.Schema(
+  {
+    status: {
+      type: String,
+      required: true
+    },
+    score: {
+      type: Number,
+      required: true
+    },
+    dateAdded: {
+      type: Date,
+      required: true
+    },
+    items: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Media',
+        required: false
+      }
+    ]
+  },
+  {
+    timestamps: true
+  }
+)
+
+module.exports = mongoose.model('MediaList', mediaListSchema)
