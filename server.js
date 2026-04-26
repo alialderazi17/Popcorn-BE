e28ed59adaddf9297a175a5bd91b204f173c239c
Ali AlDerazi <ali.alderazi0@gmail.com>
2026-04-26 18:11:06 +0300

    deploying shenanigans

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const userRouter = require('./routes/userRouter')
 
 app.use(
   cors({
-    origin: 'https://p-popcorn.netlify.app/',
+    origin: ['https://p-popcorn.netlify.app', 'http://localhost:5173'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
